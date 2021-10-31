ddffff5921b1b5295372a72c087a6e11bf8d7fbc
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-10-31 09:55:10 +0200

    Create +.py

---
+++ +.py
@@ -0,0 +1,12 @@
+import time
+
+
+t1 = time.time()
+
+for _ in range(2000):
+    print("xεΝ = 你好吗? = hospitality, boychickenRunesvaccations, Hotburger(¿), N4S<->S2N//4/00/00///--//")
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
