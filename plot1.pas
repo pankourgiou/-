0b45b83f2dacf980e48f62a5ae4859b58837d4c7
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-03-22 14:18:11 +0200

    Update plot1.pas

--- plot1.pas
+++ plot1.pas
@@ -8,3 +8,12 @@
 #     col = 'orangered', cex.lab = 0.75, cex.main = 0.8, cex.axis = 0.7)
 #dev.copy(png, file = 'plot1.png')
 #dev.off()
+# Call the script that loads and transforms the data
+#source('prepare_dataset.R')
+
+#library(png)
+# Plot the Global Active Power as a function of the date/time using
+# a continuous line, set the labels, and format the size of the fonts.
+#plot(dset$Date_time, dset$Global_active_power, type = 'l',
+#     xlab = '', ylab = 'Global Active Power (kilowatts)', cex.lab = 0.75, cex.axis = 0.75)
+#dev.copy(pn
