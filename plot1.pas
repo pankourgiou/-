cf2cc110ea9b9db8667d71033a5a1da6d0a156b6
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-03-22 14:14:46 +0200

    Update and rename +.py to plot1.pas

---
+++ plot1.pas
@@ -0,0 +1,10 @@
+# Call the script that loads and transforms the data
+#source('prepare_dataset.R')
+
+#library(png)
+# Plot a histogram for the Global Active Power, set the color,
+# the labels and the title, and format the size of the fonts.
+#hist(dset Global_active_power, xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power',
+#     col = 'orangered', cex.lab = 0.75, cex.main = 0.8, cex.axis = 0.7)
+#dev.copy(png, file = 'plot1.png')
+#dev.off()
